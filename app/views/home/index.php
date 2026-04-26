d72d1aee96e8fe3741b4954fc8a1cc566dd25e48
Hana M AbdelHamid <haniaimos6@gmail.com>
2026-04-27 01:18:39 +0300

    build: configure tailwind cli and initialize config

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -5,25 +5,25 @@
     <meta charset="UTF-8">
     <title>Welcome</title>
 
-    <!-- Bootstrap 5 CSS -->
-    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/bootstrap.min.css">
+    <!-- tailwind 4.2.1 -->
+    <link rel="stylesheet" href="<?= BASE_URL ?>assets/css/tailwind.css">
 
 </head>
 
 <body class="bg-light">
 
     <div class="container d-flex justify-content-center align-items-center min-vh-100"">
-            <div class="card shadow-lg p-5 text-center w-50">
-                <h1 class="mb-4">Welcome to My PHP MVC Project</h1>
+            <div class=" card shadow-lg p-5 text-center w-50">
+        <h1 class="mb-4">Welcome to My PHP MVC Project</h1>
 
-                <p class="lead mb-4">
-                    This is the home page.
-                </p>
+        <p class="lead mb-4">
+            This is the home page.
+        </p>
 
-                <a href="<?= BASE_URL ?>User/index" class="btn btn-primary btn-lg">
-                    Go to Users Page
-                </a>
-            </div>
+        <a href="<?= BASE_URL ?>User/index" class="btn btn-primary btn-lg">
+            Go to Users Page
+        </a>
+    </div>
     </div>
 
 </body>
