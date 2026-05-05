b112852d85c72a7dd441977d57d4318e779790d7
Hana M AbdelHamid <haniaimos6@gmail.com>
2026-05-05 05:47:36 +0300

    feat: logout logic

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -122,5 +122,13 @@ class AuthController extends Controller
 
     public function logout()
     {
+        $_SESSION = [];
+        session_destroy();
+        if (isset($_COOKIE['remember_user'])) {
+            setcookie('remember_user', '', time() - 3600, '/');
+        }
+        
+        header("Location: /login");
+        exit();
     }
 }
